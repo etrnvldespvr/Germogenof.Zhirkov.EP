e798ad3d4e71c1a4a52bec879953e64ae8480b72
Insanity <Insanity@SANITY>
2024-12-04 14:08:56 +0900

    Добавьте файлы проекта.

---
+++ Practical/Authorization.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practical
+{
+    public partial class Authorization : Page
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string.IsNullOrEmpty(LoginBox.Text) || string.IsNullOrEmpty(PasswordBox.Password)))
+            {
+                MessageBox.Show("Введите логин, пароль и капчу!");
+                return;
+            }
+            using (var db = new EducationalEntities())
+            {
+                var user = db.Users
+                    .AsNoTracking()
+                    .FirstOrDefault(u => u.Login == LoginBox.Text && u.Password == PasswordBox.Password);
+
+                if (user == null)
+                {
+                    ErrorText.Visibility = Visibility.Visible;
+                    return;
+                }
+
+                switch (user.Role)
+                {
+                    case "Teacher":
+                        //Manager.MainFrame.Navigate();
+                        break;
+                    case "Student":
+                        Manager.MainFrame.Navigate(new MainPage());
+                        break;
+                }
+            }
+        }
+    }
+}
